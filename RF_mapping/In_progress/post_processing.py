597c1393151fea1631731a0093db60efab8dbfa1
GabeLack <gabriel.lackgren@gmail.com>
2025-02-26 13:29:34 +0100

    post processing
    Homography testing done.
    Nerve-accel data done.
    
    Then basic implementation into the postprocessing class. Unittesting still required.

--- RF_mapping/In_progress/post_processing.py
+++ RF_mapping/In_progress/post_processing.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation, PillowWriter
+from matplotlib.lines import Line2D
+import seaborn as sns
 from scipy.signal import find_peaks
 import cv2
 
@@ -57,6 +60,7 @@ class DLCPostProcessing:
         self.df_monofil["Bending_Coefficient"] = bending_coefficients
 
     def plot_bending_angles(self, figsize=(10, 5)):
+        #! DRY, try collapsing the simple plotting methods
         self._get_bending_coefficients()
         plt.figure(figsize=figsize)
         plt.plot(self.df_monofil['Bending_Coefficient'], label='Bending Angle')
@@ -74,6 +78,7 @@ class DLCPostProcessing:
         return peaks
 
     def plot_bending_w_peaks(self, threshold=0.14, distance=20, figsize=(10, 5)):
+        #! DRY, try collapsing the simple plotting methods
         peaks = self.get_bending_peaks(threshold, distance)
         plt.figure(figsize=figsize)
         plt.plot(self.df_monofil['Bending_Coefficient'], label='Bending Angle')
@@ -132,9 +137,9 @@ class DLCPostProcessing:
             frames.append(frame)
 
         columns = ['tf_FR1_x', 'tf_FR1_y', 'tf_FR2_x', 'tf_FR2_y', 'tf_FG1_x', 'tf_FG1_y', 'tf_FG2_x', 'tf_FG2_y', 'tf_FB1_x', 'tf_FB1_y', 'tf_FB2_x', 'tf_FB2_y']
-        df_transformed_monofil = pd.DataFrame(transformed_monofil_points, columns=columns)
+        self.df_transformed_monofil = pd.DataFrame(transformed_monofil_points, columns=columns)
 
-        return frames, df_transformed_monofil
+        return frames, self.df_transformed_monofil
 
     def save_video(self, frames, output_path, fps=30):
         height, width, _ = frames[0].shape
@@ -145,6 +150,183 @@ class DLCPostProcessing:
             out.write(frame)
 
         out.release()
+
+
+
+    def _xlsx_processing(self, xlsx_file, original_freq = 1000, target_freq = 30):
+        # Read the Excel file
+        df_nerve = pd.read_excel(xlsx_file)
         
-    #! Next add neuron and accelerometer processing methods, especially plotting
-    #! all three together with bending angles.
+        # Extract the relevant columns #! subject to change
+        df_nerve = df_nerve.loc[:, ["Spikes nw-1-01", "5 Accelerometer", "Time"]]
+        df_nerve.columns = ["Nerve", "Accelerometer", "Time"]
+        df_nerve.dropna(inplace=True, subset=["Nerve", "Accelerometer", "Time"])
+
+        # Calculate the downsampling factor
+        downsample_factor = int(original_freq / target_freq)
+        # Apply a rolling window with a maximum function to preserve binary components
+        downsampled_df = df_nerve.rolling(window=downsample_factor, min_periods=1).max()
+        # Downsample the DataFrame by selecting every downsample_factor-th row
+        downsampled_df = downsampled_df.iloc[::downsample_factor]
+
+        self.df_external = downsampled_df
+        return downsampled_df
+
+    def plot_accelerometer_and_neuron_w_peaks(self, figsize=(10, 5), show_peaks=False):
+        fig, ax1 = plt.subplots(figsize=figsize)
+
+        # Plot accelerometer data
+        color = 'tab:blue'
+        ax1.set_xlabel('Time')
+        ax1.set_ylabel('Acceleration', color=color)
+        ax1.plot(self.df_external['Time'],
+                 self.df_external['Accelerometer'],
+                 color=color, label='Accelerometer')
+        ax1.tick_params(axis='y', labelcolor=color)
+
+        if show_peaks:
+            # Find peaks in the accelerometer data
+            peaks, _ = find_peaks(-self.df_external['Accelerometer'],
+                                prominence=0.14,
+                                distance=20)
+            ax1.plot(self.df_external['Time'].iloc[peaks],
+                    self.df_external['Accelerometer'].iloc[peaks],
+                    'x', color='green', label='contact')
+
+        # Create a second y-axis to plot neuron activity
+        ax2 = ax1.twinx()
+        color = 'tab:red'
+        ax2.set_ylabel('Neuron Activity', color=color)
+        ax2.plot(self.df_external['Time'],
+                 self.df_external['Nerve'],
+                 color=color, label='Neuron')
+        ax2.tick_params(axis='y', labelcolor=color)
+
+        # Add legends
+        fig.tight_layout()
+        fig.legend(loc='upper right',
+                   bbox_to_anchor=(1, 1),
+                   bbox_transform=ax1.transAxes)
+
+        plt.title('Accelerometer Data and Neuron Activity Over Time')
+        plt.show()
+
+    def _synchronize(self):
+        #! Implement this method such that it syncs bending coefficient with
+        #! neuron and accelerometer data. This should involve using the 5
+        #! starting stops in the neuron data to align the data.
+        
+        #! In the end, return df with neuron and accelerometer data aligned with
+        #! bending coefficient data in addition to the transformed monofilament
+        #! tip point.
+        df_rf_mapping.columns = ['tf_FB2_x',
+                                 'tf_FB2_y',
+                                 'bending_coefficient',
+                                 'neuron',
+                                 'accelerometer']
+        return df_rf_mapping
+
+    def _plot_rf_mapping(self,
+                         ax,
+                         high_bending_neuron_on,
+                         high_bending_neuron_off,
+                         low_bending_neuron_on):
+        sns.scatterplot(x='tf_FB2_x', y='tf_FB2_y',
+                        size='bending_coefficient',
+                        sizes=(50, 200), alpha=0.5,
+                        edgecolor=None,
+                        data=high_bending_neuron_on,
+                        color='red', ax=ax)
+        sns.scatterplot(x='tf_FB2_x', y='tf_FB2_y',
+                        size='bending_coefficient',
+                        sizes=(50, 200), alpha=0.5,
+                        edgecolor=None,
+                        data=high_bending_neuron_off,
+                        color='blue', ax=ax)
+        sns.scatterplot(x='tf_FB2_x', y='tf_FB2_y',
+                        size='bending_coefficient',
+                        sizes=(50, 200), alpha=0.5,
+                        edgecolor=None,
+                        data=low_bending_neuron_on,
+                        color='green', ax=ax)
+        legend_elements = [
+            Line2D([0], [0], marker='o', color='w', markerfacecolor='red',
+                   markersize=10, label='High Bending, Neuron On'),
+            Line2D([0], [0], marker='o', color='w', markerfacecolor='blue',
+                   markersize=10, label='High Bending, Neuron Off'),
+            Line2D([0], [0], marker='o', color='w', markerfacecolor='green',
+                   markersize=10, label='Low Bending, Neuron On')
+        ]
+        ax.legend(handles=legend_elements, loc='upper right')
+        ax.set_title('RF Mapping of Neuron Activity')
+        ax.set_xlabel('tf_FB2_x')
+        ax.set_ylabel('tf_FB2_y')
+        ax.invert_yaxis()
+
+    def plot_rf_mapping(self, threshold=0.14, figsize=(15, 10)):
+        threshold = threshold * self.df_rf_mapping['bending_coefficient'].max()
+        self.df_rf_mapping = self._synchronize()
+        high_bending_neuron_on = self.df_rf_mapping[
+            (self.df_rf_mapping['bending_coefficient'] > threshold) &
+            (self.df_rf_mapping['neuron'] == 1)
+        ]
+        high_bending_neuron_off = self.df_rf_mapping[
+            (self.df_rf_mapping['bending_coefficient'] > threshold) &
+            (self.df_rf_mapping['neuron'] == 0)
+        ]
+        low_bending_neuron_on = self.df_rf_mapping[
+            (self.df_rf_mapping['bending_coefficient'] <= threshold) &
+            (self.df_rf_mapping['neuron'] == 1)
+        ]
+
+        fig, ax = plt.subplots(figsize=figsize)
+        self._plot_rf_mapping(ax,
+                              high_bending_neuron_on,
+                              high_bending_neuron_off,
+                              low_bending_neuron_on)
+        plt.show()
+
+    def plot_realtime_rf_mapping(self,
+                                 threshold=0.14,
+                                 output_path='animation.gif',
+                                 fps=30,
+                                 figsize=(10, 10)):
+        threshold = threshold * self.df_rf_mapping['bending_coefficient'].max()
+        self.df_rf_mapping = self._synchronize()
+        high_bending_neuron_on = self.df_rf_mapping[
+            (self.df_rf_mapping['bending_coefficient'] > threshold) &
+            (self.df_rf_mapping['neuron'] == 1)
+        ]
+        high_bending_neuron_off = self.df_rf_mapping[
+            (self.df_rf_mapping['bending_coefficient'] > threshold) &
+            (self.df_rf_mapping['neuron'] == 0)
+        ]
+        low_bending_neuron_on = self.df_rf_mapping[
+            (self.df_rf_mapping['bending_coefficient'] <= threshold) &
+            (self.df_rf_mapping['neuron'] == 1)
+        ]
+
+        fig, ax = plt.subplots(figsize=figsize)
+
+        def init():
+            ax.clear()
+            self._plot_rf_mapping(ax,
+                                  high_bending_neuron_on.iloc[:1],
+                                  high_bending_neuron_off.iloc[:1],
+                                  low_bending_neuron_on.iloc[:1])
+            return ax
+
+        def update(frame):
+            ax.clear()
+            self._plot_rf_mapping(ax,
+                                  high_bending_neuron_on.iloc[:frame],
+                                  high_bending_neuron_off.iloc[:frame],
+                                  low_bending_neuron_on.iloc[:frame])
+            return ax
+
+        ani = FuncAnimation(fig,
+                            update,
+                            frames=len(self.df_rf_mapping),
+                            init_func=init, blit=False)
+        ani.save(output_path, writer=PillowWriter(fps=fps))
+        plt.show()
